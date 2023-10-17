692b00b24797ba64af96e850b8356d603bb91904
Smash-Enjoyer <142520767+Smash-Enjoyer@users.noreply.github.com>
2023-10-17 10:55:32 -0400

    Update lightning.py

--- lightning.py
+++ lightning.py
@@ -1,10 +1,10 @@
 def lightning(color,lColors, speed = 0.1):
-    delay = random.random()*5 + 0.5
+    delay = random.random() + 0.01
     np.fill(color)
     np.show()
     time.sleep(delay)
-    ind = random.randint(0, len(lColors)-1)
-    for i in range(random.randint(1,4)):
+    ind = random.randint(0, 1)
+    for i in range(random.randint(1,2)):
         np.fill(lColors[ind])
         np.show()
         time.sleep(0.1)
