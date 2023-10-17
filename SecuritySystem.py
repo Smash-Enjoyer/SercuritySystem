9f61de4b58183ae608be59c4355d42d6ff0c5c08
JosiahSILLS <142521126+JosiahSILLS@users.noreply.github.com>
2023-10-17 11:17:56 -0400

    Update SecuritySystem.py

--- SecuritySystem.py
+++ SecuritySystem.py
@@ -6,7 +6,7 @@ from neopixel import NeoPixel
 import random
 import adafruit_hcsr04
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D4, echo_pin=board.D5)
 
 led = dio.DigitalInOut(board.LED)
 led.direction = dio.Direction.OUTPUT
@@ -17,7 +17,7 @@ pir.direction = dio.Direction.INPUT
 button = dio.DigitalInOut(board.D7)
 button.direction = dio.Direction.INPUT
 
-bb = dio.DigitalInOut(board.D4)
+bb = dio.DigitalInOut(board.D6)
 bb.direction = dio.Direction.INPUT
 bb.pull = dio.Pull.UP
 
@@ -38,12 +38,39 @@ def disarm():
     np.fill((0,255,0))
     time.sleep(1)
     
+def blink():
+    for i in range(100):
+        delay = random.random() + 0.01
+        np.fill(red)
+        np.show()
+        time.sleep(0.5)
+        for i in range(random.randint(1,2)):
+            np.fill(red)
+            np.show()
+            time.sleep(0.1)
+            np.fill([0,0,0])
+            np.show()
+        np.fill(red)
+        np.show()
+        time.sleep(0.5)
+       
 np.fill((0,255,0))
 
 while True:
-    if not button.value and times % 2 == 0:
+    if not button.value and times == 0:
         arm()
         times += 1
-    elif not button.value and times % 2 == 1:
+    elif not button.value and times == 1:
         disarm()
-        times += 1
+        times -= 1
+    if times == 0:
+        np.fill(green)
+    else:
+        np.fill(red)
+        if bb.value == False or pir.value == True or sonar.distance < 10:
+            blink()
+            
+        
+        
+        
+        
